ac798e99cecf25f0f34e5f8727036e94841f40d5
anil sidhu <webanilsidhu@gmail.com>
2021-08-19 20:49:13 +0530

    clean

--- app.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    x:10,
-    y:20,
-    xyz:function()
-    {
-        return 10+20;
-    }
-}
--- index.js
+++
@@ -1,7 +0,0 @@
-const app=require('./app')
-let arr=[4,1,6,3,4,89,0];
-console.log(app.xyz())
-const result =arr.filter((item)=>{
-    return item>=4
-});
-console.warn(result);
