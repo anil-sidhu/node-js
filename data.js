cb58e71183c566fec0ec4d4fa1cb15f86a75eaf1
anil sidhu <webanilsidhu@gmail.com>
2021-08-30 20:51:38 +0530

    cmd input

--- data.js
+++
@@ -1,6 +0,0 @@
-const data=[
-    {name:'anil',mail:'anil@test.com'},
-    {name:'sam',mail:'peter@test.com'},
-    {name:'peter',mail:'peter@test.com'}
-]
-module.exports=data;
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const http= require('http');
-const data = require('./data');
-http.createServer((req,resp)=>{
-resp.writeHead(500,{'Content-Type':'application\json'});
-resp.write(JSON.stringify(data));
-resp.end();
-}).listen(5000);
+const fs = require('fs');
+
+const input = process.argv;
+
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4])
+}
+else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3])
+}
+else
+{
+    console.log("invalid input ")
+}
+
+
+// node index.js other red.txt 'this is color'
