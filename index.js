5b15c316e73a33a2ebfee5755dcc629c4f7412d2
anil sidhu <webanilsidhu@gmail.com>
2021-08-31 21:09:54 +0530

    clean

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const fs= require('fs');
-const path=require('path');
-const dirPath= path.join(__dirname,'files');
-console.log(dirPath)
-// for(i=0;i<5;i++)
-// {
-//     fs.writeFileSync(`${dirPath}/hello${i}.txt`,"some simple text in file")
-
-// }
-fs.readdir(dirPath,(err,files)=>{
-    files.forEach((item)=>{
-        console.warn("file name is : ",item)
-    });
-}
-)
+console.warn("CURD operations with FS")
