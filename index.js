09076918654e91231649460d86279dc8426517e1
anil sidhu <webanilsidhu@gmail.com>
2021-08-31 21:49:16 +0530

    done with curd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.warn("CURD operations with FS")
+const fs = require('fs');
+const path = require('path');
+const dirPath= path.join(__dirname,'crud');
+const filePath = `${dirPath}/apple.txt`;
+// fs.writeFileSync(filePath,'this is a simple file');
+// fs.readFile(filePath,'utf8',(err,item)=>{
+// console.log(item);
+// })
+// fs.appendFile(filePath,' for fruits',(err)=>{
+// if(!err) console.log("file is updated")
+// })
+// fs.rename(filePath, `${dirPath}/fruit.txt`,(err)=>{
+// if(!err) console.log("file name is updated")
+// })
+
+fs.unlinkSync(`${dirPath}/fruit.txt`);
