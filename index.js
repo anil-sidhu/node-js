bac51f0001e0cbe2f743c05597cb0093d31ca46a
anil sidhu <webanilsidhu@gmail.com>
2021-09-01 22:07:55 +0530

    done with basic

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const dirPath= path.join(__dirname,'crud');
-const filePath = `${dirPath}/apple.txt`;
-// fs.writeFileSync(filePath,'this is a simple file');
-// fs.readFile(filePath,'utf8',(err,item)=>{
-// console.log(item);
-// })
-// fs.appendFile(filePath,' for fruits',(err)=>{
-// if(!err) console.log("file is updated")
-// })
-// fs.rename(filePath, `${dirPath}/fruit.txt`,(err)=>{
-// if(!err) console.log("file name is updated")
-// })
+let a=20;
+let b=0;
 
-fs.unlinkSync(`${dirPath}/fruit.txt`);
+setTimeout(()=>{
+   b=30;
+},2000)
+
+
+console.log(a+b)
