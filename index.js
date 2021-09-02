63c6f7f011b63390786c019e0e6209ccdcb278a2
anil sidhu <webanilsidhu@gmail.com>
2021-09-02 21:32:50 +0530

    done with promise

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let a=20;
 let b=0;
 
-setTimeout(()=>{
-   b=30;
-},2000)
+
+let waitingData= new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+      resolve(30);
+   },2000)
+})
+
+waitingData.then((data)=>{
+   b=data;
+   console.log(a+b)
+})
 
 
-console.log(a+b)
