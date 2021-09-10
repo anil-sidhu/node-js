9d119878bf011601020ba7bbeb4bee2d4c33dc58
anil sidhu <webanilsidhu@gmail.com>
2021-09-11 01:21:52 +0530

    done with html page

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const publicPath=path.join(__dirname,'public')
 
-app.get("", (req, resp) => {
-    resp.send(`
-    <h1>Welcome to Home page</h1>
-    <a href="/about">Go to About page</a>
-    `);
-});
-
-app.get("/about", (req, resp) => {
-    resp.send(`
-    <input type="text" placeholder="user name" value="${req.query.name}"  />
-    <button>Click</button>
-    <a href="/">Go to Home page</a>
-    `);
-});
-
-app.get("/help", (req, resp) => {
-    resp.send([
-        {
-            name:'Peter',
-            email:'peter@test.com'
-        },
-        {
-            name:'Bruce',
-            email:'bruce@test.com'
-        }
-    ]);
-});
-
+app.use(express.static(publicPath));
 
 app.listen(5000);
