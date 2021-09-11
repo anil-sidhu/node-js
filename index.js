8d6e23d9a96e3bdc59bd956c51984cd6bd2ee785
anil sidhu <webanilsidhu@gmail.com>
2021-09-11 16:12:20 +0530

    done

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const path = require('path');
 const app = express();
 const publicPath=path.join(__dirname,'public')
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('',(_,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+});
+
+app.get('/contact',(_,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+});
+
+app.get('/help',(_,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('*',(_,resp)=>{
+    resp.sendFile(`${publicPath}/nopage.html`)
+});
 
 app.listen(5000);
