5165560a89f492c32e6a8fbebfd009fc40d186e4
anil sidhu <webanilsidhu@gmail.com>
2021-10-07 20:46:07 +0530

    done

--- api.js
+++ api.js
@@ -2,10 +2,19 @@ const dbConnect = require('./mongodb');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 app.get('/',async (res,resp)=>{
     let data = await dbConnect();
     data= await data.find().toArray();
     resp.send(data);
 });
+
+
+app.post("/", async (req,resp)=>{
+    let data = await dbConnect();
+    let result = await data.insert(req.body)
+    resp.send(result)
+
+})
 
 app.listen(5000)
