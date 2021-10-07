74ed9b2b78d750e50674b36d7952e63228f5fd2f
anil sidhu <webanilsidhu@gmail.com>
2021-10-07 23:12:24 +0530

    done with est

--- api.js
+++ api.js
@@ -3,18 +3,28 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.get('/',async (res,resp)=>{
+
+app.get('/', async (res, resp) => {
     let data = await dbConnect();
-    data= await data.find().toArray();
+    data = await data.find().toArray();
     resp.send(data);
 });
 
-
-app.post("/", async (req,resp)=>{
+app.post("/", async (req, resp) => {
     let data = await dbConnect();
     let result = await data.insert(req.body)
     resp.send(result)
 
+})
+
+app.put("/:name", async (req, resp) => {
+    console.log(req.body)
+    const data = await dbConnect();
+    let result = data.updateOne(
+        { name: req.params.name },
+        { $set: req.body }
+    )
+    resp.send({ status: "updated" })
 })
 
 app.listen(5000)
