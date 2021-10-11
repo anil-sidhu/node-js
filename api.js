2ad8f4191ad30877088abfffcc88c944f72ed603
anil sidhu <webanilsidhu@gmail.com>
2021-10-11 21:16:48 +0530

    done

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const dbConnect = require('./mongodb');
 const express = require('express');
+const mongodb = require('mongodb');
 const app = express();
 
 app.use(express.json());
@@ -18,13 +19,19 @@ app.post("/", async (req, resp) => {
 })
 
 app.put("/:name", async (req, resp) => {
-    console.log(req.body)
     const data = await dbConnect();
     let result = data.updateOne(
         { name: req.params.name },
         { $set: req.body }
     )
     resp.send({ status: "updated" })
+})
+
+app.delete("/:id", async (req,resp)=>{
+    console.log(req.params.id);
+    const data = await dbConnect();
+    const result = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+    resp.send(result)
 })
 
 app.listen(5000)
