bff20cf642b747cd78823ded0799c646e3d00cc4
anil sidhu <webanilsidhu@gmail.com>
2021-10-12 23:51:42 +0530

    done with curd

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const dbConnect= require('./mongodb');
+const mongoose = require('mongoose');
 
-dbConnect().then((resp)=>{
-resp.find({name:'nord'}).toArray().then((data)=>{
-console.log(data)
-})
-})
+mongoose.connect('mongodb://localhost:27017/e-comm');
+const productSchema = new mongoose.Schema({
+   name: String,
+   price: Number,
 
-const main=async ()=>{
-   let data = await dbConnect();
-   data = await data.find({name:'nord'}).toArray();
-   console.log(data)
+});
+
+const main = async () => {
+   const Product = mongoose.model('products', productSchema);
+   let data = new Product({ name: "max 131", price: 200, brand: 'xyz' });
+   const result = await data.save;
+   console.log(result);
 }
 
-main()
+const update = async () => {
+   const Product = mongoose.model('products', productSchema);
+   let data = await Product.updateOne(
+      { name: 'max pro' },
+      {
+         $set: { name: 'max pro 2', price: 1000 }
+      });
+   console.log(data);
+}
+
+const deleteData = async () => {
+   const Product = mongoose.model('products', productSchema);
+   let data = await Product.deleteOne(
+      { name: 'm' }
+      );
+   console.log(data);
+}
+
+const findData = async () => {
+   const Product = mongoose.model('products', productSchema);
+   let data = await Product.find();
+   console.log(data);
+}
+ 
+ 
+findData();
