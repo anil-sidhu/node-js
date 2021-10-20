626a6cffb79029277509415b72ad0341889aa961
anil sidhu <webanilsidhu@gmail.com>
2021-10-20 22:01:26 +0530

    done

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 require("./config");
-const Product= require('./product');
+const Product = require('./product');
 const app = express();
 
 app.use(express.json());
-app.post("/create",async (req, resp)=>{
+app.post("/create", async (req, resp) => {
     let data = new Product(req.body);
     const result = await data.save();
     resp.send(result);
-
 });
+
+app.get("/list", async (req, resp) => {
+    let data = await Product.find();
+    resp.send(data);
+})
+
+app.delete("/delete/:_id", async (req, resp) => {
+    console.log(req.params)
+    let data = await Product.deleteOne(req.params);
+    resp.send(data);
+})
+
+
+app.put("/update/:_id",async (req, resp) => {
+    console.log(req.params)
+    let data = await Product.updateOne(
+        req.params,
+        {$set: req.body}
+    );
+    resp.send(data);
+})
 
 app.listen(5000)
