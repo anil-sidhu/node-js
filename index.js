370d3a1d2d471cf964629516753ab2d8398c2f2f
anil sidhu <webanilsidhu@gmail.com>
2021-10-21 21:28:57 +0530

    done

--- index.js
+++ index.js
@@ -4,31 +4,22 @@ const Product = require('./product');
 const app = express();
 
 app.use(express.json());
-app.post("/create", async (req, resp) => {
-    let data = new Product(req.body);
-    const result = await data.save();
-    resp.send(result);
-});
 
-app.get("/list", async (req, resp) => {
-    let data = await Product.find();
+app.get("/search/:key",async (req,resp)=>{
+    let data = await Product.find(
+        {
+            "$or":[
+                {name:{$regex:req.params.key}},
+                {brand:{$regex:req.params.key}}
+            ]
+        }
+    )
     resp.send(data);
-})
 
-app.delete("/delete/:_id", async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.deleteOne(req.params);
-    resp.send(data);
 })
 
 
-app.put("/update/:_id",async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.updateOne(
-        req.params,
-        {$set: req.body}
-    );
-    resp.send(data);
-})
+
+
 
 app.listen(5000)
