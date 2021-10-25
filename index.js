e93f4adeb4694e4f3928acd32e3187163ba6be42
anil sidhu <webanilsidhu@gmail.com>
2021-10-25 21:37:02 +0530

    done with file upload

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 const express = require('express');
-require("./config");
-const Product = require('./product');
+const multer = require('multer');
+
 const app = express();
 
-app.use(express.json());
-
-app.get("/search/:key",async (req,resp)=>{
-    let data = await Product.find(
-        {
-            "$or":[
-                {name:{$regex:req.params.key}},
-                {brand:{$regex:req.params.key}}
-            ]
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads')
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
         }
-    )
-    resp.send(data);
+    })
+}).single('file_name');
 
-})
-
-
-
-
+app.post("/upload", upload, (req, resp) => {
+    resp.send("file upload")
+});
 
 app.listen(5000)
