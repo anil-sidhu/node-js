e060fbb72cfb551e72ac27b766acf4da7b9da581
anil sidhu <webanilsidhu@gmail.com>
2021-11-05 10:47:16 +0530

    done with put api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const con = require("./config");
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, resp) => {
@@ -10,12 +11,23 @@ app.get("/", (req, resp) => {
   })
 });
 
-app.post("/",(req,resp)=>{
-  const data=req.body;
-  con.query("INSERT INTO users SET?",data,(error,results,fields)=>{
-    if(error) throw error;
+
+app.post("/", (req, resp) => {
+  const data = req.body;
+  con.query("INSERT INTO users SET?", data, (error, results, fields) => {
+    if (error) throw error;
     resp.send(results)
   })
 });
+
+app.put("/:id",(req,resp)=>{
+  const data= [req.body.name,req.body.password,req.body.user_type,req.params.id];
+  con.query("UPDATE users SET name = ?, password = ?, user_type = ? WHERE id = ?",
+  data,(error,results,fields)=>{
+    if(error) throw error;
+    resp.send(results)
+  })
+ 
+})
 
 app.listen("5000")
